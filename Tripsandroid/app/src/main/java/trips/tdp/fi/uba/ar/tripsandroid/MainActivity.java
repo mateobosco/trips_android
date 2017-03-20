d46467f67b25633d151013628065596568567885
Joako <joaquin.consoni@trocafone.com>
2017-03-19 22:09:00 -0300

    View For Cities

--- Tripsandroid/app/src/main/java/trips/tdp/fi/uba/ar/tripsandroid/MainActivity.java
+++ Tripsandroid/app/src/main/java/trips/tdp/fi/uba/ar/tripsandroid/MainActivity.java
@@ -2,12 +2,33 @@ package trips.tdp.fi.uba.ar.tripsandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private BackEndClient backEndClient;
+
+    public MainActivity() {
+        this.backEndClient = new BackEndClient();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        String[] values = backEndClient.getCitiesNames();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+
+        listView.setAdapter(adapter);
+
+
     }
+
 }
