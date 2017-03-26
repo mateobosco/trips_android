eb89e67be7e1f8bde2aab609ff7535ab5133ab8f
Joako <joaquin.consoni@trocafone.com>
2017-03-26 16:01:18 -0300

    adding a filter to the city selection screen

--- Tripsandroid/app/src/main/java/trips/tdp/fi/uba/ar/tripsandroid/ListCities.java
+++ Tripsandroid/app/src/main/java/trips/tdp/fi/uba/ar/tripsandroid/ListCities.java
@@ -3,6 +3,8 @@ package trips.tdp.fi.uba.ar.tripsandroid;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -15,16 +17,16 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import org.json.*;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 
 public class ListCities extends AppCompatActivity {
     private BackEndClient backEndClient;
     private ProgressBar spinner;
-    private EditText searchBox;
+    private EditText searchEditBox;
     private ListView listView;
+    private ArrayList<String> cityNames;
+    private ArrayList<String> displayableCityNames;
 
 
     private AdapterView.OnItemClickListener mMessageClickedHandler = new AdapterView.OnItemClickListener() {
@@ -44,17 +46,17 @@ public class ListCities extends AppCompatActivity {
 
         setContentView(R.layout.activity_list);
 
-        spinner = (ProgressBar)findViewById(R.id.progressBar1);
+        spinner = (ProgressBar) findViewById(R.id.progressBar1);
         listView = (ListView) findViewById(R.id.list);
-        searchBox = (EditText) findViewById(R.id.search_box);
+        searchEditBox = (EditText) findViewById(R.id.search_box);
 
         spinner.setVisibility(View.VISIBLE);
         listView.setVisibility(View.INVISIBLE);
-        searchBox.setVisibility(View.INVISIBLE);
+        searchEditBox.setVisibility(View.INVISIBLE);
 
 
         String[] values = new String[0];
-        values = new String[] {
+        values = new String[]{
         };
 
         final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
@@ -64,16 +66,14 @@ public class ListCities extends AppCompatActivity {
         listView.setAdapter(adapter);
 
 
-
         Response.Listener<String> responseListener = new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 // Display the first 500 characters of the response string.
                 try {
-                    ArrayList<String> cityNames = new ArrayList<String>();
+                    cityNames = new ArrayList<String>();
                     JSONArray arr = new JSONArray(response);
-                    for (int i = 0; i < arr.length(); i++)
-                    {
+                    for (int i = 0; i < arr.length(); i++) {
                         String name = arr.getJSONObject(i).getString("name");
                         cityNames.add(name);
                     }
@@ -81,17 +81,19 @@ public class ListCities extends AppCompatActivity {
                     listView = (ListView) findViewById(R.id.list);
                     Collections.sort(cityNames);
 
-                    final ArrayAdapter<String> a = new ArrayAdapter<String>(ListCities.this, android.R.layout.simple_list_item_1, android.R.id.text1, cityNames);
+                    displayableCityNames = cityNames;
+
+                    final ArrayAdapter<String> a = new ArrayAdapter<String>(ListCities.this, android.R.layout.simple_list_item_1, android.R.id.text1, displayableCityNames);
 
                     listView.setAdapter(a);
                     spinner.setVisibility(View.GONE);
                     listView.setVisibility(View.VISIBLE);
-                    searchBox.setVisibility(View.VISIBLE);
+                    searchEditBox.setVisibility(View.VISIBLE);
 
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-                Log.d("exito", "Response is: "+ response);
+                Log.d("exito", "Response is: " + response);
             }
         };
         Response.ErrorListener errorListener = new Response.ErrorListener() {
@@ -105,6 +107,42 @@ public class ListCities extends AppCompatActivity {
 
 
         listView.setOnItemClickListener(mMessageClickedHandler);
+
+        searchEditBox.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                searchEditBox.setText("");
+            }
+        });
+
+
+        searchEditBox.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String query = charSequence.toString();
+                displayableCityNames = filter(cityNames, query);
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(ListCities.this, android.R.layout.simple_list_item_1, android.R.id.text1, displayableCityNames);
+                listView.setAdapter(adapter);
+            }
+
+            private ArrayList<String> filter(ArrayList<String> cityNames, String query) {
+                ArrayList<String> filtered = new ArrayList<>();
+                for (String cityName : cityNames) {
+                    if (cityName.toLowerCase().contains(query.toLowerCase())) {
+                        filtered.add(cityName);
+                    }
+                }
+                return filtered;
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
 
     }
 
