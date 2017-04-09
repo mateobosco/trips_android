f2d1447a4be8ea932bb4963cd7afdd6137268ab0
Joako <joaquin.consoni@trocafone.com>
2017-04-09 01:17:04 -0300

    diseño de reseñas

--- Tripsandroid/app/src/main/java/trips/tdp/fi/uba/ar/tripsandroid/activities/ReviewActivity.java
+++ Tripsandroid/app/src/main/java/trips/tdp/fi/uba/ar/tripsandroid/activities/ReviewActivity.java
@@ -5,13 +5,13 @@ import android.os.Bundle;
 
 import trips.tdp.fi.uba.ar.tripsandroid.R;
 
-public class ReviewsActivity extends AppCompatActivity {
+public class ReviewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reviews);
-
-        setTitle("Rating de Obelisco");
+        setContentView(R.layout.activity_review);
+        setTitle("Reseñas del obelisco");
     }
+
 }
