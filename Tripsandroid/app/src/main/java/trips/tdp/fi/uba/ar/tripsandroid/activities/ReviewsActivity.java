8fa5f95b80deb37bb1100dfa29c9efa2a51dae30
Joako <joaquin.consoni@trocafone.com>
2017-04-08 21:02:47 -0300

    creating activity

---
+++ Tripsandroid/app/src/main/java/trips/tdp/fi/uba/ar/tripsandroid/activities/ReviewsActivity.java
@@ -0,0 +1,17 @@
+package trips.tdp.fi.uba.ar.tripsandroid.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import trips.tdp.fi.uba.ar.tripsandroid.R;
+
+public class ReviewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reviews);
+
+        setTitle("Rating de Obelisco");
+    }
+}
