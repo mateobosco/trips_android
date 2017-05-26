192bec36d313d3a68347116fb4fd1e21ec93a355
Mateo Bosco <mateo.bosco@gmail.com>
2017-05-26 14:44:07 -0300

    PointsOfInterestActivity con layout

---
+++ Tripsandroid/app/src/main/java/trips/tdp/fi/uba/ar/tripsandroid/activities/PointsOfInterestActivity.java
@@ -0,0 +1,15 @@
+package trips.tdp.fi.uba.ar.tripsandroid.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import trips.tdp.fi.uba.ar.tripsandroid.R;
+
+public class PointsOfInterestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_points_of_interest);
+    }
+}
